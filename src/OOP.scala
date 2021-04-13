0f07fdda5fcbb040b32cce39e256ff57399f49f4
Abhijeet <singhabhijeet.iitkgp@gmail.com>
2021-04-13 22:07:49 +0530

    Second Commit

--- src/OOP.scala
+++ src/OOP.scala
@@ -1,11 +1,12 @@
 object OOP extends App {
   class Car{
-    val color = "Red"
-    val brand = "Honda"
-    val ModelNumber = 2
+    var color = "Red"
+    var brand = "Honda"
+    var ModelNumber = 2
     def drive():Unit = println("Congrats! for a new car")
   }
-
-
+ var newCar = new Car
+  newCar.brand = "Mersedeze"
+  println(newCar.brand)
 
 }
